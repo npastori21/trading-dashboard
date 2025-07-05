7ef44cfb6e64983bea74bdf685e4106059d16595
Nick Pastori <npastori@umich.edu>
2025-07-05 15:34:44 -0400

    start

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ chain_page = st.Page('pages/chain.py', title='Options Chain', icon='📊')
 home_page = st.Page('pages/home.py', title='Home', icon='🏠')
 strangle_page = st.Page('pages/strangle.py', title='Strangle Visualizer', icon='🏦')
 usage_page = st.Page('pages/usage.py', title='Usage', icon='🧠')
+payment_page = st.Page('pages/payments.py', title='Subscriptions', icon='💼')
 
 pg = st.navigation([home_page, chain_page, strangle_page, usage_page])
 

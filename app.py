b872b75aa9d5e061fca19bc622db509a108a6312
npastori21 <146483661+npastori21@users.noreply.github.com>
2025-07-05 20:39:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ home_page = st.Page('pages/home.py', title='Home', icon='🏠')
 strangle_page = st.Page('pages/strangle.py', title='Strangle Visualizer', icon='🏦')
 usage_page = st.Page('pages/usage.py', title='Usage', icon='🧠')
 education_page = st.Page('pages/education.py', title='Education', icon='📘')
+payment_page = st.Page('pages/payments.py', title='Subscriptions', icon='💼')
 
-pg = st.navigation([home_page, chain_page, strangle_page, education_page, usage_page])
+pg = st.navigation([home_page, chain_page, strangle_page, education_page, usage_page, payment_page])
 
 pg.run()
